799f66372dc32cdb6f63d293eb99063fc0f47530
yudaradea <yudaaradeagit@gmail.com>
2025-10-06 08:30:38 +0700

    add profile/update

--- routes/api.php
+++ routes/api.php
@@ -49,8 +49,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // Profile
     Route::get('/profile', [ProfileController::class, 'show']);
     Route::post('/profile', [ProfileController::class, 'store']);
-    Route::post('/profile', [ProfileController::class, 'update']);
-    Route::put('/profile', [ProfileController::class, 'update']);
+    Route::post('/profile/update', [ProfileController::class, 'update']);
+    Route::put('/profile/update', [ProfileController::class, 'update']);
 
     // Weight logs
     Route::apiResource('weight-logs', WeightLogController::class);
